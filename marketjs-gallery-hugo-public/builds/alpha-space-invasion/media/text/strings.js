a5556b49a7defa7fe172255c786dd80f1507368e
Andrian Alfarabi <andrian.alfarabi@impact360design.com>
2022-10-25 18:14:18 +0700

    add static game

---
+++ marketjs-gallery-hugo-public/builds/alpha-space-invasion/media/text/strings.js
@@ -0,0 +1,67 @@
+var _STRINGS = {
+	"Ad": {
+		"Mobile": {
+			"Preroll": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
+			},
+			"Header": {
+				"ReadyIn": "The game is ready in ",
+				"Loading": "Your game is loading...",
+				"Close": "Close",
+			},
+			"End": {
+				"ReadyIn": "Advertisement ends in ",
+				"Loading": "Please wait ...",
+				"Close": "Close",
+			},
+		},
+	},
+	"Splash": {
+		"Loading": "",
+		"LogoLine1": "",
+		"LogoLine2": "",
+		"LogoLine3": "",
+		"TapToStart": "TAP TO START"
+	},
+	"ToolTip": {
+		"TutorialMobile": {
+			"Steer": "to Steer",
+			"Shoot": "to Shoot"
+		},
+		"TutorialDesktop": {
+			"Steer": "Use Arrow Keys to Steer",
+			"Shoot": "Press <Space> to Shoot"
+		},
+		"ShieldTutorial": {
+			"low": "Shield is low. Avoid damage.",
+			"broken": "Shield is broken!",
+			"avoid": "Avoid damage."
+		},
+		"AsteroidWarning": {
+			"Warning": "Warning:",
+			"Evade": "Avoid Asteroids"
+		}
+	},
+	"UI": {
+		"Play": "Play Now",
+		"Settings": "Settings",
+		"Paused": "Paused",
+		"MoreGames": "More Games"
+	},
+	"Game": {
+		"Score": "Score: ",
+		"SelectPlayer": "Select Player",
+		"Win": "You win!",
+		"Lose": "You lose!",
+		"Time": "Time",
+	},
+	"Results": {
+		"GameOver": "GAME OVER",
+		"Report": "Battle Report",
+		"Enemies": "Enemies Destroyed: ",
+		"HiScore": "Hi-score: ",
+		"notSet": "Not Set"
+	},
+};
